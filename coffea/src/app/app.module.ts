6c8ce542b144671c39e6f12776c1fc954c46476c
Git-Lazzari <124463489+Git-Lazzari@users.noreply.github.com>
2023-02-06 22:33:40 +0000

    Structure Simple Site partially concluded

--- coffea/src/app/app.module.ts
+++ coffea/src/app/app.module.ts
@@ -5,13 +5,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SectionComponent } from './section/section.component';
 import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './header/menu/menu.component';
+import { ButtonComponent } from './header/menu/button/button.component';
+import { Section1Component } from './section/section1/section1.component';
+import { Section2Component } from './section/section2/section2.component';
+import { Section3Component } from './section/section3/section3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SectionComponent,
-    FooterComponent
+    FooterComponent,
+    MenuComponent,
+    ButtonComponent,
+    Section1Component,
+    Section2Component,
+    Section3Component
   ],
   imports: [
     BrowserModule,
